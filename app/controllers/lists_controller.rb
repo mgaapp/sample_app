91e8d30a3f2cdccb11ff419f8ab5218782d61da3
Ubuntu <ubuntu@ip-172-31-44-127.ap-northeast-1.compute.internal>
2026-03-15 12:19:29 +0000

    8章【バリデーションの設定】まで学習

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,22 +4,40 @@ class ListsController < ApplicationController
   end
 
   def create
-    list = List.new(list_params)
-    list.save
-    redirect_to '/top'
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list.id)
+    else
+    render :new, status: :unprocessable_entity
+    end
   end
 
   def index
+    @lists = List.all
   end
 
   def show
+    @list = List.find(params[:id])  
   end
 
-  def esit
+  def edit
+    @list = List.find(params[:id])
+  end
+
+  def destroy
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to '/lists'
+  end
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
   end
 
   private
   def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   get 'lists/new'
-  post 'lists' => 'lists#create'
-  get 'lists/index'
-  get 'lists/show'
-  get 'lists/edit'
-  get '/top' => 'homes#top'
+  get 'top' => 'homes#top'
+  resources :lists
 end
